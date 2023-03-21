e76953cf8558df8e08bbeffca4a787a20ff17f82
Your Name <you@example.com>
2023-03-21 12:58:42 +0100

    op

--- API'S/USER_API's/deposite_request.php
+++ API'S/USER_API's/deposite_request.php
@@ -1,12 +1,13 @@
 <?php
+session_start();
 include "../connect.php";
 
 // Get the user ID, transaction ID, deposit amount, deposit type, and transaction status from the form
-$user_id = $_POST['user_id'];
-$transaction_id = $_POST['transaction_id'];
+$user_id = $_SESSION['user_id'];
+$transaction_id = rand(3490,58888); 
 $amount_deposited = $_POST['amount_deposited'];
 $deposit_type = $_POST['deposit_type'];
-$transaction_status = $_POST['transaction_status'];
+$transaction_status = "pending";
 
 // Get the uploaded deposit proof image file
 $deposit_proof = file_get_contents($_FILES['deposit_proof']['tmp_name']);
