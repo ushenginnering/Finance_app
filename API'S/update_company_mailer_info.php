daa9601d8dc9bb884950077fdc68ef42d46ba74f
Your Name <you@example.com>
2023-03-21 01:14:17 +0100

    crash

--- API'S/update_company_mailer_info.php
+++ API'S/update_company_mailer_info.php
@@ -8,7 +8,7 @@ if (isset($_POST['update_mailer_info'])){
     $active_gmail_password = $_POST['active_gmail_passsword'];
     $system_auto_send_from_email_address = $_POST['system_auto_send_from_email_address'];
     $system_reply_email_address = $_POST['system_reply_email_address'];
-    $site_name = "ush Finansis";
+
 
     $sql = "UPDATE company_profile  SET welcome_mail_draft = '$welcome_draft', change_password_mail_draft = '$change_password_draft', forget_password_mail_draft = '$forget_email_draft', receive_payment_mail_draft = '$receive_patment_draft'";
    // Execute the SQL query and store the result set
