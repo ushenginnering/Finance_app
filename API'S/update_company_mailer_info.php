03eb04af678ca2d12d1fa3af8a3d356a24f46492
Your Name <you@example.com>
2023-03-21 01:00:55 +0100

    new

--- API'S/update_company_mailer_info.php
+++ API'S/update_company_mailer_info.php
@@ -10,7 +10,7 @@ if (isset($_POST['update_mailer_info'])){
     $system_reply_email_address = $_POST['system_reply_email_address'];
     $site_name = "ush Finansis";
 
-    $sql = "UPDATE company_profile  SET welcome_mail_draft = '$welcome_draft', change_password_mail_draft = '$change_password_draft', forget_password_mail_draft = '$forget_email_draft', receive_payment_mail_draft = '$receive_patment_draft' WHERE site_name = '$site_name'";
+    $sql = "UPDATE company_profile  SET welcome_mail_draft = '$welcome_draft', change_password_mail_draft = '$change_password_draft', forget_password_mail_draft = '$forget_email_draft', receive_payment_mail_draft = '$receive_patment_draft'";
    // Execute the SQL query and store the result set
     if (mysqli_query($conn, $sql)){
         echo "email drafts updated successfully";
--- API'S/update_company_meta_info.php
+++ API'S/update_company_meta_info.php
@@ -4,13 +4,12 @@ include "connect.php";
 
 if (isset($_POST['update_mailer_info'])){
     // Get form data
-    $active_gmail_address = $_POST['active_gmail_address'];
-    $active_gmail_password = $_POST['active_gmail_passsword'];
-    $system_auto_send_from_email_address = $_POST['system_auto_send_from_email_address'];
-    $system_reply_email_address = $_POST['system_reply_email_address'];
-    $site_name = "ush Finansis";
+    $site_name = $_POST['active_gmail_address'];
+    $site_url = $_POST['active_gmail_passsword'];
+    $meta_keyword = $_POST['system_auto_send_from_email_address'];
+    $meta_description = $_POST['system_reply_email_address'];
 
-    $sql = "UPDATE company_profile  SET welcome_mail_draft = '$welcome_draft', change_password_mail_draft = '$change_password_draft', forget_password_mail_draft = '$forget_email_draft', receive_payment_mail_draft = '$receive_patment_draft' WHERE site_name = '$site_name'";
+    $sql = "UPDATE company_profile  SET welcome_mail_draft = '$welcome_draft', change_password_mail_draft = '$change_password_draft', forget_password_mail_draft = '$forget_email_draft', receive_payment_mail_draft = '$receive_patment_draft' ";
    // Execute the SQL query and store the result set
     if (mysqli_query($conn, $sql)){
         echo "email drafts updated successfully";
