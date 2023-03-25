1c203dfb3a96111ed6bde360257c72197bd1c2ef
Your Name <you@example.com>
2023-03-25 21:03:01 +0100

    oil

--- API'S/USER_API's/signup.php
+++ API'S/USER_API's/signup.php
@@ -12,78 +12,93 @@ if (isset ($_POST['signup'])){
     $user_mail = $_POST['mail'] // collect the email address of the user here
     $user_id =  rand(89,234324);
 
-    if ($password == $confirm_password){
-            // SQL query to insert data into table
-            $sql = "INSERT INTO users (fullname, country, phone, password, mail, user_id) VALUES ('$fullname', '$country', '$phone', '$password', '$mail', '$user_id')";
 
-            // Execute query
-            if (mysqli_query($conn, $sql)) {
-                // echo "User signed up successfully!";
-                // send welcome mail user
-            
-                // SQL query to select the desired columns from the company_profile table
-                $sql = "SELECT welcome_mail_draft, active_gmail_address, active_gmail_password, site_name FROM company_profile";
+    
+// SQL query to select the desired columns from the company_profile table
+$sql = "SELECT * FROM users where mail  = '$user_mail'";
                 
-                // Execute the SQL query and store the result set
-                $result = mysqli_query($conn, $sql);
+// Execute the SQL query and store the result set
+$result = mysqli_query($conn, $sql);
                 
-                // Check if there are any rows returned
-                if (mysqli_num_rows($result) > 0) {
-                    // Loop through each row and output the data
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $message =  $row["welcome_mail_draft"] ;
-                        $sender_email =  $sender_gmail_email = $row["active_gmail_address"] ;
-                        $sender_gmail_password =  $row["active_gmail_password"] ;
-                        $sender_name =  $row["site_name"] ;
-                        $subject = "Welcome to ".$sender_name;
-                        include "sendemail.php";
-                        $send_email =  sendmail($sender_email,$sender_gmail_password, $sender_name, $message,$sender_gmail_email,$subject, $user_mail,$fullname);
-                        if ($send_email == 'Record saved successfully and email sent') {
-                            echo "!"; 
-                            $response = array(
-                                "registration_status"=>true,
-                                "email_status"=>true,
-                                "message"=>"Welcome Email sent successfully",
-                            );
-                            echo json_encode($response);
+// Check if there are any rows returned
+if (mysqli_num_rows($result) > 0) {
+    $response = array(
+        "registration_status"=>false,
+        "email_status"=>false,
+        "message"=>"Email already Exist",
+    );
+    echo json_encode($response);
+    }
+    else
+    {
+        if ($password == $confirm_password){
+                // SQL query to insert data into table
+                $sql = "INSERT INTO users (fullname, country, phone, password, mail, user_id) VALUES ('$fullname', '$country', '$phone', '$password', '$mail', '$user_id')";
 
-                        }else{
-                            $response = array(
-                                "registration_status"=>true,
-                                "email_status"=>false,
-                                "message"=>"Email could not be sent",
-                            );
-                            echo json_encode($response);
+                // Execute query
+                if (mysqli_query($conn, $sql)) {
+                    // echo "User signed up successfully!";
+                    // send welcome mail user
+                
+                    // SQL query to select the desired columns from the company_profile table
+                    $sql = "SELECT welcome_mail_draft, active_gmail_address, active_gmail_password, site_name FROM company_profile";
+                    
+                    // Execute the SQL query and store the result set
+                    $result = mysqli_query($conn, $sql);
+                    
+                    // Check if there are any rows returned
+                    if (mysqli_num_rows($result) > 0) {
+                        // Loop through each row and output the data
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $message =  $row["welcome_mail_draft"] ;
+                            $sender_email =  $sender_gmail_email = $row["active_gmail_address"] ;
+                            $sender_gmail_password =  $row["active_gmail_password"] ;
+                            $sender_name =  $row["site_name"] ;
+                            $subject = "Welcome to ".$sender_name;
+                            include "sendemail.php";
+                            $send_email =  sendmail($sender_email,$sender_gmail_password, $sender_name, $message,$sender_gmail_email,$subject, $user_mail,$fullname);
+                            if ($send_email == 'Record saved successfully and email sent') {
+                                //echo "!"; 
+                                $response = array(
+                                    "registration_status"=>true,
+                                    "email_status"=>true,
+                                    "message"=>"Welcome Email sent successfully",
+                                );
+                                echo json_encode($response);
+                            }else{
+                                $response = array(
+                                    "registration_status"=>true,
+                                    "email_status"=>false,
+                                    "message"=>"Email could not be sent",
+                                );
+                                echo json_encode($response);
+                            }
                         }
+                    } else {
+                        $response = array(
+                            "registration_status"=>true,
+                            "email_status"=>false,
+                            "message"=>"No results found",
+                        );
+                        echo json_encode($response);
                     }
                 } else {
                     $response = array(
-                        "registration_status"=>true,
-                        "email_status"=>false,
-                        "message"=>"No results found",
+                        "registration_status"=>false,
+                        "message"=>"Error signing up user",
                     );
+                    // Output an error message
                     echo json_encode($response);
                 }
-            } else {
-                $response = array(
-                    "registration_status"=>false,
-                    "message"=>"Error signing up user",
 
-                );
-                // Output an error message
-                echo json_encode($response);
-            }
-
-    }else{
-        $response = array(
-            "registration_status"=>false,
-            "message"=>"Password does not match",
-
-        );
-        // Output an error message
-        echo json_encode($response);
+        }else{
+            $response = array(
+                "registration_status"=>false,
+                "message"=>"Password does not match",
+            );
+            // Output an error message
+            echo json_encode($response);
+        }
     }
-
-
 }
 ?>
